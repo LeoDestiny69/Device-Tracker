f550a68316759b0a833546f39d44874ae41fd973
Tinnaphop <74139538+LeoDestiny69@users.noreply.github.com>
2025-08-14 01:46:20 +0700

    update settings

--- device_tracker/tracker_project/settings.py
+++ device_tracker/tracker_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = "tracker_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.parent / "device_tracker_frontend" / "dist"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
